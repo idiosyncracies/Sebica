424bd5be3e6bf7a0fc54a3b94679aded7a9bb925
idiosyncracies <liu67662121@163.com>
2024-10-30 00:40:42 +0800

    Third commitment
    Solve the bug in the arch file.

--- models/mini_arch.py
+++ models/mini_arch.py
@@ -5,44 +5,35 @@ from thop import profile
 from thop import clever_format
 import time
 
-class CSA(nn.Module):
-    def __init__(self, channel, kernel_size=3, reduction = 16,):
-        super(CSA, self).__init__()
+
+class SECA(nn.Module):  # Spatial-Efficient Channel Attention (SECA)
+    def __init__(self, channel, reduction=8):  ###16 or 8(mini)
+        super(SECA, self).__init__()
         self.spatial_attention = nn.Sequential(
             nn.Conv2d(2, 1, kernel_size=7, padding=3, bias=False),
             nn.Sigmoid()
         )
-        # Bi-Directional
-        self.channel_attention_forward = nn.Sequential(
+        self.channel_attention = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
-            nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size-1)//2, bias=False),  # 1D卷积（正向）
-            nn.Sigmoid()
-        )
-        self.channel_attention_backward = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
-            nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size-1)//2, bias=False),  # 1D卷积（反向）
+            nn.Conv2d(channel, channel // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, bias=False),
             nn.Sigmoid()
         )
 
-    def forward(self, x):  #(B,C,H,W)
-        # Spatial attention
-        avg_out = torch.mean(x, dim=1, keepdim=True) # (B,1,H，W）
-        max_out, _ = torch.max(x, dim=1, keepdim=True) # (B,1,H，W）
-        spatial_weight = torch.cat([avg_out, max_out], dim=1) # (B,2,H，W）
-        spatial_weight = self.spatial_attention(spatial_weight) # (B,1,H，W）
+    def forward(self, x):
+        # spacial attn:
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        spatial_weight = torch.cat([avg_out, max_out], dim=1)
+        spatial_weight = self.spatial_attention(spatial_weight)
 
-        # Bi-Directional attention
-        b, c, _, _ = x.size()
-        y = torch.mean(x, dim=(2, 3), keepdim=True)  # （B，16,1,1）全局平均池化后的通道向量
-
-        y_forward = self.channel_attention_forward(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1) ## （B，1,1,1）
-        y_backward = self.channel_attention_backward(y.squeeze(-1).transpose(-1, -2).flip(dims=[1])).transpose(-1, -2).unsqueeze(-1) ##（B，1,1,1）
-
-        channel_weight = (y_forward + y_backward.flip(dims=[1])) / 2
-        channel_weight = channel_weight.expand_as(x) ## （B，16,H，W）
+        # channel attn (2 MLP)
+        channel_weight = self.channel_attention(x)
 
         out = x * spatial_weight * channel_weight
         return out
+
 
 class Conv(nn.Module):
     def __init__(self, N):
@@ -78,24 +69,24 @@ class FFN(nn.Module):
 class Attn(nn.Module):
     def __init__(self, N):
         super().__init__()
-        self.pre_mixer = Conv(N)
-        self.post_mixer = FFN(N)
-        self.attn = CSA(N, reduction=16)
+        self.token_mixer = Conv(N)
+        self.channel_mixer = FFN(N)
+        self.attn = SECA(N, reduction=8)
         self.norm1 = nn.BatchNorm2d(N)
         self.norm2 = nn.BatchNorm2d(N)
 
     def forward(self, x):
-        out = self.pre_mixer(x)
+        out = self.token_mixer(x)
         out = self.norm1(out)
         out = self.attn(out)
-        out = self.post_mixer(out)
+        out = self.channel_mixer(out)
         out = self.norm2(out)
-        out += x
+        out += x  # 添加残差连接
         return out
 
 
 class RTSR(nn.Module):
-    def __init__(self, sr_rate=4, N=16):  ### 16 or 8: mini
+    def __init__(self, sr_rate=4, N=8):  ### 16 or 8: mini
         super(RTSR, self).__init__()
         self.scale = sr_rate
 
@@ -106,7 +97,7 @@ class RTSR(nn.Module):
         )
 
         self.body = nn.Sequential(
-            *[Attn(N) for _ in range(6)] ### for mini is 4
+            *[Attn(N) for _ in range(4)]
         )
 
         self.tail = nn.Sequential(
@@ -130,17 +121,21 @@ class RTSR(nn.Module):
 
         return out
 
-if __name__ == '__main__':
-    model = RTSR(sr_rate=4, N=16) ### 16, 8
 
+if __name__ == '__main__':
+    model = RTSR(sr_rate=4, N=8)  ### 16, 8
+
+    # Params
     params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f'Number of trainable parameters: {params}')
 
+    # GFLOPs
     input = torch.randn(1, 3, 224, 224)
     macs, params = profile(model, inputs=(input,))
     macs, params = clever_format([macs, params], "%.3f")
     print(f'GFLOPs: {macs}')
 
+    # runtime
     input_tensor = torch.randn(1, 3, 224, 224).cuda()
     model = model.cuda()
     start_time = time.time()
